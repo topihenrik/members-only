a1ab81754142e58bc674ced3a4e8392def7abf6a
topihenrik <trashtunnus@gmail.com>
2022-08-20 13:40:52 +0300

    Visuals have been improved

--- models/message.js
+++ models/message.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema(
     {
@@ -9,5 +10,10 @@ const MessageSchema = new Schema(
         author: {type: Schema.Types.ObjectId, ref: "User"}
     }
 )
+
+// fix this
+MessageSchema.virtual("timestamp_formatted").get(function() {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_SHORT);
+})
 
 module.exports = mongoose.model("Message", MessageSchema);
